5cbbe54fc827ffc74283b7327aadb1f1dd3e9f6a
Martin0o0 <8546088@naver.com>
2023-11-26 16:24:31 +0900

    todolist 프로젝트

---
+++ todolist/src/components/TodoHeader.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  name: "TodoHeader"
+}
+</script>
+
+<template>
+  <header>
+    <h1>TodoList</h1>
+  </header>
+</template>
+
+<style scoped>
+
+h1{
+  color: black;
+  font-weight: 900;
+  margin: 2.5rem 0 1.5rem;
+}
+
+
+</style>
---
+++ todolist/src/components/TodoInput.vue
@@ -0,0 +1,64 @@
+<script>
+export default {
+  name: "TodoInput",
+  data() {
+    return {
+      newTodoItem: ''
+    }
+  },
+  methods:{
+    addTodo(){
+      if (this.newTodoItem !== ""){
+        let value = this.newTodoItem && this.newTodoItem.trim();
+        console.log(this.newTodoItem);
+        localStorage.setItem(this.newTodoItem, this.newTodoItem);
+        this.$emit('addTodo', value);
+        this.clearInput();
+      }
+    },
+    clearInput(){
+      this.newTodoItem = "";
+    },
+
+  }
+}
+</script>
+
+<template>
+  <div class="inputBox shadow">
+    <input type="text" v-model="newTodoItem" placeholder="Type what you have to do" v-on:keyup.enter="addTodo">
+    <span class="addContainer" v-on:click="addTodo">
+      <i class="addBtn fa fa-plus" aria-hidden="true"></i>
+    </span>
+  </div>
+</template>
+
+<style scoped>
+input:focus{
+  outline:none;
+}
+
+.inputBox{
+  background-color: white;
+  height: 50px;
+  line-height: 50px;
+  border-radius : 5px;
+}
+.inputBox input{
+  border-style: none;
+  font-size: 0.9em;
+}
+
+.addContainer{
+  float: right;
+  background: linear-gradient(to right, #6478FB,#8763FB );
+  display: inline-block;
+  width: 3rem;
+  border-radius: 0 5px 5px 0;
+ }
+.addBtn{
+  color: white;
+  vertical-align: middle;
+}
+
+</style>
